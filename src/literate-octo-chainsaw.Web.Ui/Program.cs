027e9f434c06690e3f60c471c66683047666590a
Bobby Yasumura <byasumura@bdo.ca>
2017-02-24 16:39:28 -0500

    Add project files.

---
+++ src/literate-octo-chainsaw.Web.Ui/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace literate_octo_chainsaw.Web.Ui
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
