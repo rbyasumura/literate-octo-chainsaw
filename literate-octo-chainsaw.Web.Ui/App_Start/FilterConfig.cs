8a852a990d5dbc4fba778db6504e00068b671420
Bobby Yasumura <byasumura@bdo.ca>
2017-02-24 18:32:54 -0500

    Added .net framework project

---
+++ literate-octo-chainsaw.Web.Ui/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace literate_octo_chainsaw.Web.Ui
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ literate-octo-chainsaw.Web.Ui/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="literate_octo_chainsaw.Web.Ui.MvcApplication" Language="C#" %>
