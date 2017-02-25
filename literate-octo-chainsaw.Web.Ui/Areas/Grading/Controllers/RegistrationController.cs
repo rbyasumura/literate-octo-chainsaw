7c7224203d26f3f0d6baf9f30e7549c183fa65e0
Bobby Yasumura <byasumura@bdo.ca>
2017-02-24 19:04:01 -0500

    Updated packages

---
+++ literate-octo-chainsaw.Web.Ui/Areas/Grading/Controllers/RegistrationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace literate_octo_chainsaw.Web.Ui.Areas.Grading.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Grading/Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ literate-octo-chainsaw.Web.Ui/Areas/Grading/GradingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace literate_octo_chainsaw.Web.Ui.Areas.Grading
+{
+    public class GradingAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Grading";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Grading_default",
+                "Grading/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
